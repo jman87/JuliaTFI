2fe201a6dc552cfe1ce3d4c7f64e23437d2e4fb2
jman87 <jman87@users.noreply.github.com>
2023-01-15 09:42:33 -0600

    General updates

--- src/block.jl
+++ src/block.jl
@@ -1,6 +1,7 @@
 IntOrFloat = Union{Int, Float64}
 VI = Vector{Int}
 VF = Vector{Float64}
+VIF = Union{Vector{Int}, Vector{Float64}}
 VMI = Union{Vector{Int}, Matrix{Int}}
 VMIF = Union{Vector{Int}, Vector{Float64}, Matrix{Int}, Matrix{Float64}}
 
@@ -75,6 +76,78 @@ function block(I::VMI, J::VMI, K::VMI, x::VMIF, y::VMIF, z::VMIF)
 
   return X
 end
+
+@with_kw struct SphereType
+center::VIF
+radius::Float64
+type::String="SPHERE"
+end
+
+function sphere(
+  point_x::IntOrFloat, point_y::IntOrFloat, point_z::IntOrFloat,
+  radius::IntOrFloat
+  )
+  return SphereType(
+    center=[ Float64(point_x), Float64(point_y), Float64(point_z) ],
+    radius=Float64(radius)
+  )
+end
+
+function project_to_sphere!(ijk::Int, val::Int, X::Array{Float64,4}, sphere::SphereType)
+
+  # Note that for projecting a surface, the 4D array X, is passed to this
+  # function as a 3D array, i.e., a surface rather than a volume.
+
+  local Xijk
+  if ijk==1
+    Xijk = X[:,val,:,:]
+  elseif ijk==2
+    Xijk = X[:,:,val,:]
+  elseif ijk==3
+    Xijk = X[:,:,:,val]
+  end
+
+  for j = 1:size(Xijk, 3)
+    for i = 1:size(Xijk, 2)
+      x = Xijk[:,i,j]
+      Xijk[:,i,j] = nearest_point_on_sphere(x, sphere)
+    end
+  end
+
+  if ijk==1
+  X[:,val,:,:] = Xijk
+  elseif ijk==2
+    X[:,:,val,:] = Xijk
+  elseif ijk==3
+    X[:,:,:,val] = Xijk
+  end
+
+  return X
+end
+
+function nearest_point_on_sphere(point_p::VF, sphere::SphereType)
+
+  # Compute Point q on Sphere Closest to Point p
+
+  xc = sphere.center[1]
+  yc = sphere.center[2]
+  zc = sphere.center[3]
+
+  xp = point_p[1]
+  yp = point_p[2]
+  zp = point_p[3]
+
+  cp = sqrt( (xp-xc)*(xp-xc) + (yp-yc)*(yp-yc) + (zp-zc)*(zp-zc) )
+
+  ratio = sphere.radius / cp
+
+  xq = xc + (ratio * (xp-xc))
+  yq = yc + (ratio * (yp-yc))
+  zq = zc + (ratio * (zp-zc))
+  
+  return [xq, yq, zq]
+end
+
 
 # function block(I::VMI, J::VMI, K::VMI, x::VMIF, y::VMIF, z::VMIF)
   
--- test.jl
+++ test.jl
@@ -1,8 +1,19 @@
 using JuliaTFI
 using WriteVTK
 
-X = block([1, 5], [1, 9],  [1, 4],
-          [0, 4], [-2, 2], [-1, 1])
+X = block(
+    [1, 3], [1, 3],  [1, 3],
+    [0, 4], [-2, 2], [-2, 2]
+)
+
+sd1 = sphere(0,0,0, 8)
+
+println("BEFORE"); display(X)
+
+Y = project_to_sphere!(1, size(X,2), X, sd1)
+
+println("AFTER"); display(X)
+println("Y"); display(Y)
 
 vtkfile = vtk_grid("vtk01", X)
 outfiles = vtk_save(vtkfile)
